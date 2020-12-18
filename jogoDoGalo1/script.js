517f26bc1b2ab6f2ccc4e66b859e010597e4679c
AndreGoncalves222 <andrevianagoncalves02@gmail.com>
2020-12-18 09:53:21 +0000

    jogoDoGalo

---
+++ jogoDoGalo1/script.js
@@ -0,0 +1,12 @@
+let symbol = 'X'
+        const tds = document.querySelectorAll('td')
+        for (const td of tds) {
+            td.addEventListener('click', function () {
+                if (this.innerHTML == '') {
+                    symbol=='O' ? symbol = 'X' : symbol = 'O' 
+                    this.innerHTML = symbol
+                } else {
+                    alert('Ocupado!')
+                }
+            })
+        }
