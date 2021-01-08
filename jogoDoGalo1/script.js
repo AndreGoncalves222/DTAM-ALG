1c01833250c847cf50593fc243901f626cf54667
AndreGoncalves222 <andrevianagoncalves02@gmail.com>
2021-01-08 11:21:36 +0000

    try

--- jogoDoGalo1/script.js
+++ jogoDoGalo1/script.js
@@ -1,12 +1,19 @@
 let symbol = 'X'
-        const tds = document.querySelectorAll('td')
-        for (const td of tds) {
-            td.addEventListener('click', function () {
-                if (this.innerHTML == '') {
-                    symbol=='O' ? symbol = 'X' : symbol = 'O' 
-                    this.innerHTML = symbol
-                } else {
-                    alert('Ocupado!')
-                }
-            })
+const tds = document.querySelectorAll('td')
+for (const td of tds) {
+    td.addEventListener('click', function () {
+        if (this.innerHTML == '') {
+            symbol == 'O' ? symbol = 'X' : symbol = 'O'
+            this.innerHTML = symbol
+        } else {
+            alert('Ocupado!')
         }
+    })
+    
+
+}
+
+/* let addcell = document.querySelector('table')
+    if (symbol '') {
+        
+    } */
